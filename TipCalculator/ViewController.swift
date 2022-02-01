44f639bcb0e160ad17e46742eaf4d697e6636028
ayerose <reya-k@outlook.de>
2022-02-01 19:34:35 +0000

    added functionality

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -2,14 +2,14 @@
 //  ViewController.swift
 //  TipCalculator
 //
-//  Created by Soreya Koura on 01.02.22.
+//  Created by Rey on 01.02.22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    
+    // allocate btns
     @IBOutlet weak var AmountTextField: UITextField!
     @IBOutlet weak var AmountOfPerson: UITextField!
     @IBOutlet weak var PercentLabel: UILabel!
@@ -19,6 +19,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var TotalCostsLabel: UILabel!
     @IBOutlet weak var CostsPerPerson: UILabel!
     
+    var foodCosts : Double = 0.0
+    var tipPercent : Double = 0.0
+    var tipSum : Double = 0.0
+    var totalCosts : Double = 0.0
+    var costsPerPerson : Double = 0.0
+    
+    
     
  override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,21 +33,28 @@ class ViewController: UIViewController {
     }
     
     
+    // add funtionality to percent btns
     
-    
+    // return = leave this block after user clicked on a btn and go to next block
     @IBAction func PercentBtnAction(_ sender: UIButton) {
         
         if sender.titleLabel?.text == "3%" {
             PercentLabel.text = "3%"
             PercentSlider.value = 3.0
+            calculateCosts()
+            return
         }
         if sender.titleLabel?.text == "5%" {
             PercentLabel.text = "5%"
             PercentSlider.value = 5.0
+            calculateCosts()
+            return
         }
         if sender.titleLabel?.text == "7%" {
             PercentLabel.text = "7%"
             PercentSlider.value = 7.0
+            calculateCosts()
+            return
         }
     }
     
@@ -49,8 +63,33 @@ class ViewController: UIViewController {
         let percentToString = String(format: "%.0f", sender.value)
         // add %
         PercentLabel.text = percentToString + "%"
+        
+         calculateCosts()
     }
     
+    func calculateCosts() {
+        foodCosts  = Double(AmountTextField.text!)!
+        tipPercent = Double(PercentSlider.value)
+        
+        tipSum = (foodCosts * tipPercent) / 100
+        totalCosts = foodCosts + tipSum
+        
+        costsPerPerson = totalCosts / Double(AmountOfPerson.text!)!
+        
+        printUIElements()
+    }
+    
+    // render calculated costs on iphone
+    func printUIElements(){
+        // takes the value of tipSum and formates it to text which has 2 nachkommastellen
+        let stringTip = String(format: "%.2f", tipSum)
+        let stringTotalCost = String(format: "%.2f", totalCosts)
+        let stringCostsPerson = String(format: "%.2f", costsPerPerson)
+        
+        TipLabel.text = "Tip: " + stringTip + "€"
+        TotalCostsLabel.text = "Total Costs: " + stringTotalCost + "€"
+        CostsPerPerson.text = "Costs Per person: " +  stringCostsPerson + "€"
+    }
 
 }
 
