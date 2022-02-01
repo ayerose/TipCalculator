825cee9eff9b51c247448ff6a92deab1884cdecd
ayerose <reya-k@outlook.de>
2022-02-01 18:45:31 +0000

    added functionality

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -32,13 +32,23 @@ class ViewController: UIViewController {
         
         if sender.titleLabel?.text == "3%" {
             PercentLabel.text = "3%"
+            PercentSlider.value = 3.0
         }
         if sender.titleLabel?.text == "5%" {
             PercentLabel.text = "5%"
+            PercentSlider.value = 5.0
         }
         if sender.titleLabel?.text == "7%" {
             PercentLabel.text = "7%"
+            PercentSlider.value = 7.0
         }
+    }
+    
+    // sender is the UI slider, %.0f = null nachkommastellen
+    @IBAction func PercentSliderAction(_ sender: UISlider) {
+        let percentToString = String(format: "%.0f", sender.value)
+        // add %
+        PercentLabel.text = percentToString + "%"
     }
     
 
