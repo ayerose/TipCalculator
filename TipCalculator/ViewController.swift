569e901d2396beb12c0925d2bff9b6f84411125b
ayerose <reya-k@outlook.de>
2022-02-01 18:32:31 +0000

    added labels

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    
+    @IBOutlet weak var AmountTextField: UITextField!
+    @IBOutlet weak var AmountOfPerson: UITextField!
+    @IBOutlet weak var PercentLabel: UILabel!
+    
+    @IBOutlet weak var PercentSlider: UISlider!
+    @IBOutlet weak var TipLabel: UILabel!
+    @IBOutlet weak var TotalCostsLabel: UILabel!
+    @IBOutlet weak var CostsPerPerson: UILabel!
+    
+    
+ override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    
+    @IBAction func PercentBtnAction(_ sender: UIButton) {
+        
+        if sender.titleLabel?.text == "3%" {
+            PercentLabel.text = "3%"
+        }
+        if sender.titleLabel?.text == "5%" {
+            PercentLabel.text = "5%"
+        }
+        if sender.titleLabel?.text == "7%" {
+            PercentLabel.text = "7%"
+        }
+    }
+    
 
 }
 
